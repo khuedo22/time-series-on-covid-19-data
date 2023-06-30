b5673765727cc030e14bfd18b7db5cd372cf54de
khuedo22 <khuedothi@outlook.com>
2023-06-29 22:45:15 -0700

    updated and cleaned up some code

--- time series.R
+++ time series.R
@@ -19,46 +19,47 @@ data<- read_csv("states.timeseries.csv")
 
 #subset data to only include washington state
 washington <- data %>% select(state, date, actuals.newCases) %>% filter(state == "WA") %>% filter(actuals.newCases != "NA")
+
+#transform into ts object
 washington$date <- as.Date(washington$date)
-plot(washington$date, washington$actuals.newCases,type="l")
 washington_xts <- xts(washington$actuals.newCases, washington$date)
 washington_xts_subset <- window(washington_xts, start="2021-1-01", end="2021-12-31")
-plot(washington_xts_subset)
 washington_ts <- ts(washington_xts_subset, frequency=7)
+
+
+#train/test split
 washington_training <- subset(washington_ts, end = length(washington_ts)-16)
 washington_testing <- subset(washington_ts, start=length(washington_ts)-15)
-#convert actuals.cases to time series
 
 
+#plotting training data
+plot(washington_training, xlab="Date", ylab="New Cases", main="New Covid-19 Cases in Washington")
 
-washington_plot <- plot(washington_ts, ylab= "New Cases", xlab="Time", main = "New COVID-19 cases for Washington State")
 
-washington_ts_sma <- SMA(washington_ts, n=25)
-washington_ts_sma_plot <- plot.ts(washington_ts_sma)
+#decomposing data into components
 
-washington_ts_decompose <- decompose(washington_ts)
+washington_ts_decompose <- decompose(washington_training)
 washington_ts_decompose_plot <- plot(washington_ts_decompose)
 plot(washington_ts_decompose)
 
 
-
-
-# using sarima
+# building SARIMA model
 
 ndiffs(washington_training)
 washington_diff <- diff(washington_training)
-plot(washington_diff)
+plot(washington_diff, ylab="Differenced Cases", main="Difference of 1")
 adf.test(washington_diff)
 ggtsdisplay(washington_diff)
 
 
-fit6 <- Arima(washington_training, order=c(2, 1, 3), seasonal=c(3, 0, 0)) 
-fit6 %>% checkresiduals()
-fit7 <- Arima(washington_training, order=c(5, 1, 3), seasonal=c(3, 0, 0)) 
-fit7 %>% checkresiduals()
-fit8 <- Arima(washington_training, order=c(5, 1, 5), seasonal=c(3, 0, 0)) 
-fit8 %>% checkresiduals()
+fit1 <- Arima(washington_training, order=c(2, 1, 3), seasonal=c(3, 0, 0)) 
+fit1 %>% checkresiduals()
+fit2 <- Arima(washington_training, order=c(5, 1, 3), seasonal=c(3, 0, 0)) 
+fit2 %>% checkresiduals()
+fit3 <- Arima(washington_training, order=c(5, 1, 5), seasonal=c(3, 0, 0)) 
+fit3 %>% checkresiduals()
 
+#comparing model with testing data
 fit7 %>% forecast(h=15) %>% autoplot() + autolayer(washington_testing)
 fit6 %>% forecast(h=15) %>% autoplot() + autolayer(washington_testing)
 
@@ -66,6 +67,9 @@ autoplot(washington_training, series="training") + autolayer(fitted(fit6, h=15),
 test <- Arima(washington_testing, model=fit6)
 accuracy(test)
 
+
+
+#extra
 auto_fit <- auto.arima(washington_diff)
 residuals <- checkresiduals(auto_fit)
 auto_fit %>% forecast(h=15) %>% autoplot() + autolayer(washington_testing)
